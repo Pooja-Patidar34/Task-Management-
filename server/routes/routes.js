e3212c2550b1bd510a3af215d457132d04c9f149
Pooja-Patidar34 <pooja.chsr85@gmail.com>
2025-11-16 00:10:53 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import userRoutes from "./userRoutes.js";
 import taskRoutes from "./taskRoutes.js";
-import {verifyCookies} from '../utils/index.js'
 
 const router = express.Router();
 router.use("/user", userRoutes);
- router.use("/task",verifyCookies, taskRoutes);
+ router.use("/task", taskRoutes);
 export default router;
